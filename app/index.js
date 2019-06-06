7eec458da0b27fc03e67067c81e81288f250fbe1
Spencer Simons <spencersimons@Spencers-MacBook-Pro.local>
2019-06-05 23:03:42 -0600

    added headers, string decoder for PostData as buffer

--- app/index.js
+++ app/index.js
@@ -4,17 +4,33 @@
 
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 var server = http.createServer(function(req,res){
   var parsedUrl = url.parse(req.url, true);
   var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
-  var method = req.method.toLowerCase();
+  var queryStringObject = parsedUrl.query; //handles query string parameters like /?fizz=buzz
 
-  res.end('Hello World!\n');
+  var method = req.method.toLowerCase();//http method
+  var headers = req.headers;//headers as object
+  var decoder = new StringDecoder('utf-8'); // payload handling
+  var buffer = '';
+  req.on('data',function(data){
+    buffer += decoder.write(data);
+  });
+  req.on('end',function(){
+    buffer += decoder.end();
 
-  console.log('request received on this path: '+trimmedPath+' with this method: '+method);
+    res.end('Hello World!\n');
+
+    console.log('path: '+trimmedPath);
+    console.log(' method: '+method);
+    console.log(' query string parameters:',queryStringObject);
+    console.log('headers: ',headers);
+    console.log('payload: ',buffer);
+  });
 });
 
 server.listen(3000, function(){
